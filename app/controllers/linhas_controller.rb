b7be2cfde10132761a740a5dc151ab1e6863653f
Jean Carlos Will <jean.will@softfocus.com.br>
2016-06-11 15:16:45 -0300

    .

--- app/controllers/linhas_controller.rb
+++ app/controllers/linhas_controller.rb
@@ -3,4 +3,8 @@ class LinhasController < ApplicationController
   def index
      render json: Linha.all.to_json(include: :pontos)
    end
+
+   def details
+     render json: Linha.find(params[:id]).to_json(include: :pontos)
+   end
 end
