12772d79a8c40c64f4ce81694c708d738649f6d6
Jean Carlos Will <jean.will@softfocus.com.br>
2016-06-11 15:27:34 -0300

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-l = Linha.create(nome: "Linha sao jorge")
-l.pontos.create lat: "23.2323", lon: "23.32"
-l.pontos.create lat: "33.3", lon: "31.32"
-l.pontos.create lat: "31.232123233", lon: "22.332"
+#l = Linha.create(nome: "Linha sao jorge")
+#l.pontos.create lat: "23.2323", lon: "23.32"
+#l.pontos.create lat: "33.3", lon: "31.32"
+#l.pontos.create lat: "31.232123233", lon: "22.332"
+Linha.create(nome: "Av. Tupi - UTFPR/FADEP")
+Linha.create(nome: "Av. Tupi - Alvorada")
+Linha.create(nome: "Av. Tupi - São Cristovão")
+Linha.create(nome: "Novo Horizonte")
+Linha.create(nome: "Gralha Azul")
+Linha.create(nome: "Guarani")
+Linha.create(nome: "Planalto")
+Linha.create(nome: "UTFPR - Centro")
+Linha.create(nome: "UTFPR - La Salle")
